2cfa3016f9e29b540401ae2b610a62d570110613
Fan Yansen <fanyansen20@gmail.com>
2026-09-12 17:14:19 +0700

    error

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -2,6 +2,6 @@ import { addNumber } from "@macrospace-parking/sample-lib"
 // import Image from "next/image"
 
 export default function Home() {
-  addNumber(1, 2)
+  addNumber(1, 2```)
   return <>Hello World</>
 }
